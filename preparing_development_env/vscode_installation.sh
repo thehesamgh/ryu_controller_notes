bb2706d195f1157ce9b5fa9a58e7b0e8b13f8788
Hessam Ghassemi <44605128+hqasemi@users.noreply.github.com>
2020-05-28 23:26:09 +0430

    Update vscode_installation.sh

--- preparing_development_env/vscode_installation.sh
+++ preparing_development_env/vscode_installation.sh
@@ -1,1 +1,12 @@
 sudo snap install --classic code # or code-insiders
+
+
+#or
+sudo apt install ./<file>.deb
+
+# If you're on an older Linux distribution, you will need to run this instead:
+# sudo dpkg -i <file>.deb
+# sudo apt-get install -f # Install dependencies
+
+
+#for more info check the following link: https://code.visualstudio.com/docs/setup/linux
