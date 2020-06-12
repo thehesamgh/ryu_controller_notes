b6ac7cbdc5394285ee99c934e297b4ff8f5c5941
Hessam Ghassemi <s.hesam.ghasemi@gmail.com>
2020-06-11 21:19:08 -0700

    delete some files

--- preparing_development_env/mininet_installation.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/mininet/mininet
-cd mininet/utils
-./install.sh -fnv
-
-
-#persian manual --> https://sdncentral.ir/1398/08/02/how-to-solve-mininet-installation-issues/
--- preparing_development_env/ryu_installation.sh
+++
@@ -1,4 +0,0 @@
-pip install eventlet msgpack-python netaddr oslo.config routes six webob
-git clone git://github.com/osrg/ryu.git
-cd ryu
-python ./setup.py install
